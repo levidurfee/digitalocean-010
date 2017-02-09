01272fff6787cfdb9e6a359481dbd955266210ad
levidurfee <levi.durfee@gmail.com>
2017-02-09 08:38:44 -0500

    [Account] add main class

---
+++ src/Account.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace wappr\digitalocean;
+
+class Account extends Resources
+{
+    /**
+     * @var Sender
+     */
+    protected $sender;
+
+    private $endpoint = 'images';
+
+    public function get()
+    {
+        
+    }
+}
